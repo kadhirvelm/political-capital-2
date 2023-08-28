3740d76fdda6b6032adb064dda712ed624260c2c
Kadhir Mani <kadhirvelm@gmail.com>
2023-08-27 23:09:23 -0700

    Add comments

--- packages/pc2-cli/src/generateTypeFiles.ts
+++ packages/pc2-cli/src/generateTypeFiles.ts
@@ -6,6 +6,7 @@ import { glob } from "glob";
 import { ensureDirSync, emptyDirSync } from "fs-extra";
 import { watch } from "chokidar";
 import { generateOutputTypeFile } from "./generateOutputTypeFile.js";
+import chalk from "chalk";
 
 const OUTPUT_DIRECTORY = "css_types";
 const SCSS_GLOB_PATTERN = "**/*.module.scss";
@@ -18,18 +19,26 @@ const prepareOutput = () => {
 };
 
 export async function generateTypeFiles(shouldWatch: boolean) {
+  const startPerformance = performance.now();
+  console.log(chalk.yellow("Preparing output directory"));
   prepareOutput();
 
+  console.log(chalk.yellow("Gathering all SCSS modules files"));
   const relativePathToModuleFiles = await getCurrentScssModuleFiles();
   for (const relativePath of relativePathToModuleFiles) {
     generateOutputTypeFile(OUTPUT_DIRECTORY, relativePath);
+    console.log(chalk.green("Generated file ", relativePath));
   }
+
+  console.log(chalk.blue("Completed in ", Math.round((performance.now() - startPerformance) * 10) / 10, " ms"));
 
   if (!shouldWatch) {
     return;
   }
 
   watch(SCSS_GLOB_PATTERN).on("change", (path) => {
+    console.log(chalk.yellow("Identified changed SCSS modules path ", path));
     generateOutputTypeFile(OUTPUT_DIRECTORY, path);
+    console.log(chalk.green("Finished regenerating type file"));
   });
 }
